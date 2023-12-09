b48e7f5fb3ebed7afdccdcd01f218f9191f26d65
Alion <alion.111114@gmail.com>
2023-12-09 09:51:06 +0530

    transcribe script updated

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import io
 import speech_recognition as sr
 from pydub import AudioSegment
-
+import requests
 # Load environment variables from .env file
 load_dotenv()
 
@@ -19,9 +19,35 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 lighthouse_api_token = os.environ.get('LIGHTHOUSE_API_TOKEN')
 lh = Lighthouse(token=lighthouse_api_token)
 
-def transcribe_video(video_path):
-    # Load the video file and convert it to WAV format
-    video = AudioSegment.from_file(video_path, format="mp4")
+
+def generate_summary(transcript):
+    # Append "please make a summary of it" to the transcript
+    prompt = transcript + "\n\n what is this video for"
+
+    # Use OpenAI GPT-3 API to generate a summary
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150,  # Adjust the max_tokens parameter based on your preference
+        temperature=0.6  # Adjust the temperature parameter based on your preference
+    )
+
+    summary = response.choices[0].text.strip()
+    return summary
+
+
+
+def transcribe_video_from_url(video_url):
+    # Download the video from the URL
+    video_data = requests.get(video_url).content
+
+    # Save the video data to a file
+    video_file_path = "video.mp4"
+    with open(video_file_path, "wb") as video_file:
+        video_file.write(video_data)
+
+    # Convert the video to WAV format
+    video = AudioSegment.from_file(video_file_path, format="mp4")
     audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2)
     audio.export("audio.wav", format="wav")
 
@@ -34,6 +60,10 @@ def transcribe_video(video_path):
 
     # Recognize the speech in the audio
     text = r.recognize_google(audio_text, language='en-US')
+
+    # Remove the temporary files
+    os.remove(video_file_path)
+    os.remove("audio.wav")
 
     # Return the transcribed text
     return text
@@ -51,13 +81,13 @@ def summarize_video():
     video_url = request.json.get('video_url')
 
     # Transcribe the video
-    transcript = transcribe_video(video_url)
+    transcript = transcribe_video_from_url(video_url)
 
     # Save the transcript as a local file
     transcript_filename = save_transcript_to_file(video_url, transcript)
 
     # Upload the transcript file to Lighthouse
-    lh.upload_file(file_path=transcript_filename)
+    upload = lh.upload(source=transcript_filename)
 
     # Generate a summary using OpenAI GPT-3
     summary = generate_summary(transcript)
@@ -74,20 +104,4 @@ def summarize_video():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-def generate_summary(transcript):
-    # Append "please make a summary of it" to the transcript
-    prompt = transcript + "\n\n what is this video for"
-
-    # Use OpenAI GPT-3 API to generate a summary
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150,  # Adjust the max_tokens parameter based on your preference
-        temperature=0.6  # Adjust the temperature parameter based on your preference
-    )
-
-    summary = response.choices[0].text.strip()
-    return summary
-
 
